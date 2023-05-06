1f009427262e786d0f89de0a24b99eb92773ae13
KENR <zero6305@gmail.com>
2023-05-06 21:04:21 +0300

    Added cat, write, sha256, duplicate checker(dups)

--- explorer-dsu/main.js
+++ explorer-dsu/main.js
@@ -14,6 +14,8 @@ const templateSSI = keyssispace.createTemplateSeedSSI('default');
 
 // other requirements
 const fs = require('fs');
+const crypto = require('crypto');
+const pathModule = require('path');
 
 
 let data = { "message": "Hello world!" };
@@ -24,6 +26,7 @@ const readline = require('readline').createInterface({
     input: process.stdin,
     output: process.stdout
 });
+
 
 /**
  * Function that interprets the input from the user and executes the command
@@ -107,6 +110,60 @@ function processCommand(input, dsuInstance) {
                     }
                 });
             }
+        case 'write':
+            let filePathToWrite = args[1];
+            let contentToWrite = args[2];
+
+            writeToDsu(dsuInstance, filePathToWrite, contentToWrite);
+            break;
+        case 'cat':
+            let filePathToRead = args[1];
+
+            // filePathToRead = getAbsolutePath(filePathToRead);
+            
+            getFileContents(dsuInstance, filePathToRead).then((content) => {
+
+                let humanReadableContent = content;
+
+                console.log('========== File contents: =========')
+                console.log(humanReadableContent);
+                console.log('========== End of content =========')
+            });
+
+            break;
+        case 'sha256':
+            let filePathToHash = args[1];
+
+            filePathToRead = getAbsolutePath(filePathToRead);
+
+            calculateHashForFile(dsuInstance, filePathToHash).then((hash) => {
+                console.log(`SHA256 hash of file ${filePathToHash}: ${hash}`);
+            });
+            break;
+
+        case 'dups':
+            let directoryToCheck = getAbsolutePath(args[1] ?? currentDirectory);
+
+            getDuplicates(dsuInstance, directoryToCheck)
+            .then((duplicates) => {
+
+                let duplicatesCounts = 0;
+                let currentGroupId = 1;
+
+                for (let [key, value] of duplicates) {
+                    console.log(key + ' = ' + value);
+
+                    if (value.length > 1) {
+                        console.log(`=== Duplicates group ${currentGroupId++}: ${value.length} files with same content ===`);
+                        console.log(value.join('\t'));
+                        duplicatesCounts += 1;
+                    }
+                }
+
+                console.log(`Total unique files duplicated (once or multiple): ${duplicatesCounts}`);
+
+            });
+            break;
         case 'pwd':
             console.log(currentDirectory);
             break;
@@ -145,6 +202,139 @@ function promptCommand(dsuInstance) {
     });
 }
 
+async function writeToDsu(dsuInstance, path, content) {
+
+    files = await listFilesFromDsu(dsuInstance, path);
+
+    if (files.includes(path)) {
+        console.log(`File ${path} already exists. Overwriting...`);
+
+    } else {
+        console.log(`File ${path} does not exist. Creating...`);
+    }
+
+    let fileToCreate = getAbsolutePath(path);
+    dsuInstance.writeFile(fileToCreate, content, (err) => {
+        if (err) {
+            console.error(err);
+        } else {
+            console.log(`Created file ${fileToCreate}`);
+        }
+    });
+}
+
+async function getFileContents(dsuInstance, path) {
+
+    let outerPath = pathModule.basename(getAbsolutePath(path));
+
+    existentFiles = await listFilesFromDsu(dsuInstance, outerPath);
+
+    console.log(`Existent files in ${outerPath} : `)
+    console.log(existentFiles);
+
+    if (existentFiles.includes(path)) {
+        console.log(`File ${path} exists. Reading from it...`);
+    } else {
+        console.log(`File ${path} does not exist. Aborting...`);
+    }
+
+    return new Promise((resolve, reject) => {
+        dsuInstance.readFile(path, (err, data) => {
+            if (err) {
+                console.error(`Error reading file ${path}: ${err}`);
+                reject(err);
+            } else {
+                console.log(`Read file ${path}`);
+
+                var humanReadableContent = data.toString('utf8');
+
+                resolve(humanReadableContent);
+            }
+        });
+    });
+
+}
+
+
+// momentan recursive nu face nimic (sau asa pare)
+async function listFilesFromDsu(dsuInstance, path, recursive = false, includeFolders = true) {
+
+    return new Promise((resolve, reject) => {
+
+        let targetDir = path;
+
+        dsuInstance.readDir(targetDir, { recursive: recursive,
+            withFileTypes : true,
+         }, (err, entries) => {
+            if (err) {
+                console.error(`Error listing files: ${err}`);
+
+                reject(err);
+
+                throw err;
+            } else {
+
+                let results = [];
+
+                results = results.concat(entries.files);
+
+                if (includeFolders) {
+                    results.concat(entries.directories);
+                }
+
+                console.log(`Listing files:\n${entries.files.join('\n')}`);
+
+                resolve(results);
+            }
+        });
+    });
+}
+
+
+async function getDuplicates(dsuInstance, path) {
+
+    files = await listFilesFromDsu(dsuInstance, path, true, false);
+
+    let hashToFilesMap = new Map();
+
+    for (let file of files) {
+        let hash = await calculateHashForFile(dsuInstance, getAbsolutePath(file));
+
+
+        if (hashToFilesMap.has(hash)) {
+            let processedFilesWithHash = [];
+
+            processedFilesWithHash = hashToFilesMap.get(hash);
+
+            processedFilesWithHash.push(file);
+
+            hashToFilesMap[hash] = processedFilesWithHash;
+        } else {
+            hashToFilesMap.set(hash, [file]);
+        }
+    }
+
+    return hashToFilesMap;
+}
+
+
+async function calculateHashForFile(dsuInstance, path) {
+
+    return new Promise((resolve, reject) => {
+
+        getFileContents(dsuInstance, path).then((content) => {
+
+            console.log(`Calculating hash for file ${path}`);
+            console.log(`File content: ${content}`);
+
+            let hash = crypto.createHash('sha256').update(content).digest('hex');
+
+            resolve(hash);
+        });
+
+    });
+}
+
 async function initializeToDefaultDsuCreateIfNotExists() {
 
 
@@ -156,7 +346,7 @@ async function initializeToDefaultDsuCreateIfNotExists() {
     //check if the file exists
 
 
-    if (dsu_s.dsus.length > 0) {
+    if (dsu_s && "dsus" in dsu_s && dsu_s.dsus.length > 0) {
         //if it does, load the DSU
 
         resolver.loadDSU(dsu_s.dsus[0].key, (err, dsuInstance) => {
@@ -169,6 +359,10 @@ async function initializeToDefaultDsuCreateIfNotExists() {
     }
     else {
         //if it doesn't, create a new DSU and save key to dsu_s.json
+
+        dsu_s = {
+            dsus: []
+        }
 
         // dsuInstance = await $$.promisify(resolver.createDSU)(templateSSI)
         dsuInstance = await new Promise((resolve, reject) => {
